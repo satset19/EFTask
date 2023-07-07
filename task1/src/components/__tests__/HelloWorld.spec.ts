cacd4a30e5ad02828c52c952c1c24cee674717ff
satset19 <laksonosatiyo@gmail.com>
2023-07-07 19:08:41 +0700

    test

--- task1/src/components/__tests__/HelloWorld.spec.ts
+++ task1/src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+// import { mount } from '@vue/test-utils'
+// import DatePicker from '../DatePicker.vue'
 
 describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
+  // it('renders properly', () => {
+  //   const wrapper = mount(DatePicker, { props: { msg: 'Hello Vitest' } })
+  //   expect(wrapper.text()).toContain('Hello Vitest')
+  // })
 })
